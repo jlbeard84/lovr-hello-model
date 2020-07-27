640cd49aec699ebd3e7ae582cd06dd1c78fbc587
Josh Beard <josh@jlbeard.com>
2020-07-27 18:55:42 -0400

    Added base for lighting

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ Loading a simple model in lovr
 
 ### Model
 Model courtesty of ftql8d on [Sketchfab](https://sketchfab.com/3d-models/dragon-quest-slime-d310bea62e374815ab10939a35952535)
+
+### Lighting
+Lighting code courtesy of [Barely Conscious Games](https://barelyconsciousgames.blogspot.com/2020/07/simple-lighting-for-lovr-phong-model-in.html)
--- main.lua
+++ main.lua
@@ -1,9 +1,25 @@
+customVertex = [[
+    vec4 position(mat4 projection, mat4 transform, vec4 vertex)
+    {
+        return projection * transform * vertex;
+    }
+]]
+
+customFragment = [[
+    vec4 color(vec4 graphicsColor, sampler2D image, vec2 uv)
+    {
+        return graphicsColor * lovrDiffuseColor * vertexColor * texture(image, uv);
+    }
+]]
+
+local shader
+
 function lovr.load()
+    shader = lovr.graphics.newShader(customVertex, customFragment, {})
     model = lovr.graphics.newModel('slime.obj')
 end
 
 function lovr.draw()
-    lovr.graphics.setWireframe(true)
+    lovr.graphics.setShader(shader)
     model:draw(0, 1.7, -3, 1, lovr.timer.getTime() * .25)
-    lovr.graphics.setWireframe(false)
 end
